6dd42e790cc5cf6a4c33f9c1b825e0ed1bd9113a
linhao <linhao.4zhong@163.com>
2018-08-28 11:26:46 +0800

    修复bug

--- src/getBranch.ts
+++ src/getBranch.ts
@@ -4,8 +4,8 @@ var path = require( 'path' );
 export = function getBranch(): string {
     let branch = '';
     const cwd = process.cwd();
-    if ( fs.existsSync( path.join( cwd, '../.git/HEAD' ) ) ) {
-        let file = fs.readFileSync( path.join( cwd, '../.git/HEAD' ), 'utf8' );
+    if ( fs.existsSync( path.join( cwd, './.git/HEAD' ) ) ) {
+        let file = fs.readFileSync( path.join( cwd, './.git/HEAD' ), 'utf8' );
         let arr = file.split( '/' );
         branch = arr[arr.length - 1].replace( /\s+/g, '' ).split( '\\' )[0];
     }
